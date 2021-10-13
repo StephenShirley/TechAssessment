9f43e3b57f82838f723e28231327b67b8430d335
Stephen <stephen1341@gmail.com>
2021-10-13 13:07:18 -0500

    removed old component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TreeComponent } from './tree/tree.component';
 import { NodeComponent } from './node/node.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TreeComponent,
     NodeComponent
   ],
   imports: [
